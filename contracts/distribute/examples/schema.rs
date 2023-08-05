ad6ed45cf0a3c071097b6415eb4704e7d70c4c62
smith <smithmonnnnnnn@gmail.com>
2023-08-05 12:31:00 +0800

    update schema

--- contracts/distribute/examples/schema.rs
+++ contracts/distribute/examples/schema.rs
@@ -1,6 +1,6 @@
 use cosmwasm_schema::write_api;
 
-use kpt_distribute::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use distribute::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     write_api! {
--- contracts/seilor/examples/schema.rs
+++ contracts/seilor/examples/schema.rs
@@ -1,6 +1,6 @@
 use cosmwasm_schema::write_api;
 
-use kpt::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use seilor::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
 
 fn main() {
     write_api! {
